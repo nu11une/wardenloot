82d879c9c2761a01d6f21154a119c6ddd10d992a
Bagg6168 <nulu0265@gmail.com>
2022-06-18 19:11:53 -0400

    Fixed LivingEntityMixin

--- src/main/java/net/nu11une/wardenloot/mixin/LivingEntityMixin.java
+++ src/main/java/net/nu11une/wardenloot/mixin/LivingEntityMixin.java
@@ -1,12 +1,10 @@
 package net.nu11une.wardenloot.mixin;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.nu11une.wardenloot.core.WLHelmet;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,31 +14,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-    @Inject(method = "setStatusEffect", at = @At("HEAD"), cancellable = true)
-    public void setStatusEffectCallback(StatusEffectInstance effect, Entity source, CallbackInfo ci) {
-        if (source != null) {
-            if (setEffect(effect, source)) ci.cancel();
-        }
+    @Inject(method = "setStatusEffect", at = @At("TAIL"), cancellable = true)
+    public void setStatusEffectCallback(CallbackInfo ci) {
+        LivingEntity entity = (LivingEntity) (Object) this;
+        setEffect(entity);
     }
 
-    @Inject(method = "addStatusEffect(Lnet/minecraft/entity/effect/StatusEffectInstance;Lnet/minecraft/entity/Entity;)Z", at = @At("HEAD"), cancellable = true)
-    public void addStatusEffectCallback(StatusEffectInstance effect, Entity source, CallbackInfoReturnable<Boolean> cir) {
-        if (source != null) {
-            cir.setReturnValue(setEffect(effect, source));
-        }
+    @Inject(method = "addStatusEffect", at = @At("TAIL"), cancellable = true)
+    public void addStatusEffectCallback(CallbackInfoReturnable<Boolean> cir) {
+        LivingEntity entity = (LivingEntity) (Object) this;
+        setEffect(entity);
     }
 
-    public boolean setEffect(StatusEffectInstance effect, Entity source) {
-        if (source.isPlayer()) {
-            if(source instanceof ServerPlayerEntity){
-                for (ItemStack stack : source.getArmorItems()) {
-                    Item item = stack.getItem();
-                    if (item.equals(WLHelmet.SCULK_HELMET) && effect.getEffectType().equals(StatusEffects.DARKNESS)) {
-                        return false;
-                    }
+    @Inject(method = "onEquipStack", at = @At("HEAD"), cancellable = true)
+    public void onEquipStackCallback(CallbackInfo ci) {
+        LivingEntity entity = (LivingEntity) (Object) this;
+        setEffect(entity);
+    }
+
+    public void setEffect(LivingEntity entity) {
+        if(entity instanceof PlayerEntity){
+            for (ItemStack stack : entity.getArmorItems()) {
+                Item item = stack.getItem();
+                if (item.equals(WLHelmet.SCULK_HELMET)) {
+                    entity.removeStatusEffect(StatusEffects.DARKNESS);
                 }
             }
         }
-        return true;
     }
 }
